146a7b928435c52d51f3f73cee6b43fbe7af7708
Ashish kumar palai <ashishkumarpalai2000@gmail.com>
2023-07-13 13:34:07 +0530

    we have deployement

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
@@ -113,5 +114,8 @@ def view_orders():
 
 
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
